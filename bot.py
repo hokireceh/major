e602c214977930b0989b32ddd648cecbffa26fa8
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-09-02 13:16:16 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import sys
 
 sys.dont_write_bytecode = True
 
-from hokireceh-claimer import base
+from hokireceh_claimer import base
 from core.token import get_token
 from core.info import get_balance
 from core.task import process_check_in, process_do_task
